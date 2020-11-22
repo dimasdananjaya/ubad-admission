122a9634c294e4f8ad58a682904412c292b0e5d6
dimasdananjaya <gededimas@gmail.com>
2020-11-22 21:04:58 +0800

    initial commit and migration

---
+++ resources/views/home.blade.php
@@ -0,0 +1,168 @@
+@extends('layouts.app')
+
+@section('content')
+<section id="mahasiswa-baru">
+    <div data-aos="fade-left" class="col-lg-12 maba-header">
+        <h2 data-aos="fade-left" class="text-center"><b>Pendaftaran Mahasiswa Baru Dibuka !</b></h2>
+        <hr>
+        <p style="text-align:justify" class="text-center card-text" data-aos="fade-left" class="why-dosen lead">
+            Daftarkan dirimu sekarang untuk bergabung dengan Universitas Bali Dwipa,
+            10 Program Studi pilihan tersedia untukmu. <br>Ikuti tata cara pendaftaran dibawah untuk melakukan pendaftaran
+        <p>
+
+        <img class="mx-auto d-block hero-image" src="/resources/logo/mahasiswa-baru.svg">
+    </div>
+
+    <div id="merdeka-belajar">
+        <div class="container-fluid">
+            <h3 class="text-center mb-3 mt-4"><b>Kampus Merdeka, Merdeka Belajar !</b></h3>
+            <p class="text-center mb-5">Dalam mendukung program Merdeka Belajar oleh Kementerian Pendidikan dan Kebudayaan Republik Indonesia dan mensupport 
+                calon mahasiswa baru <br> dalam masa Pandemi COVID-19 ini Universitas Bali Dwipa
+                memberikan <b>5 Kebebasan</b> bagi Calon Mahasiswa Baru diantaranya : </p>
+            <div class="row">
+                <div class="col-lg-4 mt-5">
+                    <img class="mx-auto d-block" src="/resources/logo/bebas-uang-pendaftaran.svg">
+                    <h5 class="text-center mt-2"><b>Bebas Uang Pendaftaran</b></h5>
+                    <p class="text-center">Pendaftaran Online Gratis ! <br> Anda dapat langsung mendaftar tanpa membayar </p>
+                </div><!--col-lg-4-->
+
+                <div class="col-lg-4 mt-5">
+                    <img class="mx-auto d-block" src="/resources/logo/bebas-waktu-pendaftaran.svg">
+                    <h5 class="text-center mt-2"><b>Bebas Waktu Pendaftaran</b></h5>
+                    <p class="text-center">Pendaftaran Online Dimanapun Kapanpun <br> Anda dapat langsung mendaftar dimanapun dan kapanpun secara online </p>
+                </div><!--col-lg-4-->
+
+                <div class="col-lg-4 mt-5">
+                    <img class="mx-auto d-block" src="/resources/logo/bebas-waktu-kuliah.svg">
+                    <h5 class="text-center mt-2"><b>Bebas Waktu Kuliah</b></h5>
+                    <p class="text-center">Perkuliahan Dapat Dilakukan Online <br> Anda dapat mengikuti kuliah secara Online kami memiliki sistem E-Learning yang akan
+                    membantu kalian dalam melakukan perkuliahan dimanapun dan kapanpun</p>
+                </div><!--col-lg-4-->
+
+                <div class="col-lg-6 mt-5">
+                    <img class="mx-auto d-block" src="/resources/logo/bebas-waktu-bayar-kuliah.svg">
+                    <h5 class="text-center mt-2"><b>Bebas Waktu Bayar Kuliah</b></h5>
+                    <p class="text-center">Punya Kendala Dalam Pembayaran? <br> Pembayaran Uang Kuliah dapat dibayar kapanpun <br>dan dicicil hingga sebelum Semester berakhir </p>
+                </div><!--col-lg-4-->
+
+                <div class="col-lg-6 mt-5">
+                    <img class="mx-auto d-block" src="/resources/logo/bebas-kuliah-3-semester.svg">
+                    <h5 class="text-center mt-2"><b>Bebas Kuliah 3 Semester Diluar Prodi</b></h5>
+                    <p class="text-center">Perkuliahan Merdeka Belajar <br> Mahasiswa berhak mengambil 3 semester diluar program studinya sesuai dengan prgoram merdeka balajar</p>
+                </div><!--col-lg-4-->
+
+                <div class="col-lg-12">
+                    <a href="/form-pendaftaran" class="btn btn-primary btn-md text-center mx-auto d-block mt-4" style="width: 23em; background-color:#40a8c4; border-radius:1em 1em 1em 1em;">Berminat Bergabung? <br> Klik Disini Untuk Daftar Online</a>
+                </div><!--div class-->
+            </div><!--row-->
+        </div><!--container-fluid-->
+    </div><!--merdeka belajar-->
+
+    <div class="container">
+        <!-- Full Page Image Header with Vertically Centered Content -->
+        <div class="row">
+            <div class="col-lg-12">
+                <hr>
+                <h3 class="text-center mt-4"><b>Alur Pendaftaran</b></h3>
+                <img class="img-responsive" src="/resources/logo/alur-pendaftaran.svg" alt="Card image cap">
+            </div><!--col 12-->
+
+            <div class="col-lg-6">
+                <div class="card">
+                    <img class="card-img-top" src="/resources/logo/isi-formulir.svg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title"><b>1. Isi Formulir Online</b></h5>
+                        <p class="card-text">Isi formulir online dengan menekan tombol dibawah, pastikan data yang anda isi sesuai, Setelah mengisi formulir secara online admin kami akan memproses formulir dan akan menghubungi via Whatsapp
+                            dalam waktu 24 jam</p>
+                        <a href="/form-pendaftaran" class="btn btn-primary btn-md text-center">Isi Formulir Pendaftaran Disini</a>
+                    </div><!--card body-->
+                </div><!--card-->
+            </div><!--col 6-->
+
+            <div class="col-lg-6">
+                <div class="card">
+                    <img class="card-img-top" src="/resources/logo/admin.svg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title"><b>*Admin Akan Segera Menghubungi Anda*</b></h5>
+                        <p class="card-text">
+                            Setelah mengisi formulir Online maka salah satu dari admin kami akan menghubungi anda, admin kami nantinya
+                            akan membantu anda hingga proses akhir pendaftaran
+                        </p>
+                    </div><!--card body-->
+                </div><!--card-->
+            </div><!--col 6-->
+
+            <div class="col-lg-6">
+                <div class="card">
+                    <img class="card-img-top" src="/resources/logo/diproses.svg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title"><b>2. Penyerahan Persyaratan</b></h5>
+                        <p class="card-text">
+                            Calon manahasiswa baru menyerahkan kelengkapan berkas persyaratan pendaftaran berupa : <br>
+                            1. Ijazah (Legalisir)<br>
+                            2. Rapor (Legalisir)<br>
+                            3. KTP
+                            <br>
+                            <small><b>*seluruh persyaratan dikirim ke email pmb@balidwipa.ac.id </b></small>
+                        </p>
+                        <a href="/status-pendaftaran" class="btn btn-primary btn-md text-center">Cek Status Pendaftaran</a>
+                    </div><!--card body-->
+                </div><!--card-->
+            </div><!--col 6-->
+
+            <div class="col-lg-6">
+                <div class="card">
+                    <img class="card-img-top" src="/resources/logo/pembayaran.svg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title"><b>3. Pembayaran Pendaftaran dan Initial Payment</b></h5>
+                        <p class="card-text">Setelah data divalidasi selanjutnya calon mahasiswa wajib membayar <b>Biaya Pendaftaran dan Initial Payment</b>
+                        jumlah biaya bergantung pada Program Studi yang dipilih, hubungi admin kami dengan menekan tombol Whatsapp di pojok kanan bawah
+                        untuk mengetahui biaya yang harus dibayar. Bukti pembayaran dapat dibawa langsung ke Universitas Bali Dwipa atau dikirimkan online melalui admin kami
+                        yang telah menghubungi anda.
+                        <br><small><b>*tata cara pembayaran akan diberikan setelah mengumpulkan berkas pendaftaran kepada admin</b></small>
+                        </p>
+                    </div><!--card body-->
+                </div><!--card-->
+            </div><!--col 6-->
+
+            <div class="col-lg-6">
+                <div class="card">
+                    <img class="card-img-top" src="/resources/logo/registrasi-ulang.svg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title"><b>4. Registrasi Ulang</b></h5>
+                        <p class="card-text">Setelah melakukan <b>Pembayaran Pendaftaran dan Initial Payment (3)</b> selanjutnya calon mahasiswa wajib melakukan <b>Registrasi Ulang</b>
+                        Registrasi Ulang adalah proses administrasi yang <b>Wajib</b> dilakukan oleh seluruh calon mahasiswa, proses ini mencakup melengkapi administrasi, pengumpulan
+                        dokumen, dan pelunasan DP3 dan Semester. 
+                        </p>
+                    </div><!--card body-->
+                </div><!--card-->
+            </div><!--col 6-->
+
+            <div class="col-lg-6 d-flex mx-auto">
+                <div class="card">
+                    <img class="card-img-top" src="/resources/logo/selesai.svg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title" style="color : #4e89ae;"><b>5. SELESAI</b></h5>
+                        <p class="card-text">Setelah calon Mahasiswa Baru menyelesaikan seluruh proses 1-4, maka <b>SELAMAT !</b> Anda telah terdaftar resmi sebagai Calon Mahasiswa Universitas Bali Dwipa. 
+                            Calon mahasiswa menunggu untuk pengumuman Masa Orientasi</p>
+                        <a href="/status-pendaftaran" class="btn btn-primary btn-md text-center">Cek Status Pendaftaran</a>
+                    </div><!--card body-->
+                </div><!--card-->
+            </div><!--col 6-->
+
+            
+        </div><!--row-->
+    </div><!--container-->
+    <div class="mahasiswa-baru-question">
+        <div data-aos="fade-right">
+            <h2 class="text-center">Ada Pertanyaan?</h2>
+            <p class="card-text text-center">Tekan tombol Whatsapp di pojok kanan bawah <br>atau Hubungi Kami Pada Kontak Dibawah Ini</p>
+            <ul class="text-center">
+                <li>Whatsapp : 085792463944 </li>
+                <li>Phone    : 081339827770</li>
+                <li>Email    : info@balidwipa.ac.id</li>
+            </ul>
+        </div>
+    </div><!--question-->
+</section>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,171 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Universitas Bali Dwipa merupakan perguruan tinggi
+    di bali dengan 10 Program Studi S1 dengan Kelas Karyawan dan Kelas Reguler engan 10 Program Studi Pilihan : Teknik Biomedis, 
+    Teknologi Pangan, Farmasi, Psikologi, Hubungan Masyarakat, Akuntansi, 
+    Bahasa Inggris, Sistem Informasi, Gizi, Hukum
+    ">
+    <meta name=”robots” content="index, follow">
+    	
+    <link rel='icon' href='/resources/logo/balidwipa.png'>
+    <link rel="shortcut icon" href="/resources/logo/balidwipa.png" />
+    <link rel="apple-touch-icon" href="/resources/logo/balidwipa.png" />
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Universitas Bali Dwipa</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://kit.fontawesome.com/4f161c1c95.js" crossorigin="anonymous"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/style.css?v=1') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+    <!--font-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round|Raleway" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+
+    <!--AOS-->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+    <!-- Add the slick-theme.css if you want default styling -->
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css" />
+    <!-- Add the slick-theme.css if you want default styling -->
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css" />
+
+    <!--Sweet alert-->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+
+    <script src="https://unpkg.com/@popperjs/core@2"></script>
+
+</head>
+
+<body>
+   
+        <nav class="navbar navbar-default navbar-expand-lg navbar-light fixed-top">
+            <div class="navbar-header d-flex col">
+                <a class="navbar-brand" href="#"><img src="/resources/logo/balidwipa.png"
+                        class="navbar-logo" />Admission Bali Uni    
+                </a>
+                <button type="button" data-target="#navbarCollapse" data-toggle="collapse"
+                    class="navbar-toggle navbar-toggler ml-auto">
+                    <span class="navbar-toggler-icon"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <!-- Collection of nav links, forms, and other content for toggling -->
+            <div id="navbarCollapse" class="collapse navbar-collapse justify-content-start">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item"><a href="/home" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="/home" class="nav-link">Login</a></li>
+                    <li class="nav-item"><a href="/home" class="nav-link">Daftar</a></li>
+
+                    <li class="nav-item dropdown">
+                        <a data-toggle="dropdown" class="nav-link dropdown-toggle" href="#">Akademik <b
+                                class="caret"></b></a>
+                        <ul class="dropdown-menu">
+
+                        </ul>
+                    </li>
+
+                </ul>
+                <ul class="nav navbar-nav navbar-right ml-auto">
+                    <li class="nav-item">
+                        <a href="https://www.facebook.com/ubadbali/" class="nav-link"><i
+                                class="fab fa-facebook-square"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://www.instagram.com/universitasbalidwipa/" class="nav-link"><i
+                                class="fab fa-instagram"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        @yield('content')
+        @include('sweetalert::alert')
+        <div class="row">
+            <div class="col-lg-12">
+                <hr>
+            </div>
+        </div>
+        <footer class="fdb-block footer-large">
+        <div class="container">
+            <div class="row text-center">
+            <div class="col">
+                <a style="color: black;" class="navbar-brand" href="#"><img src="/resources/logo/balidwipa.png"
+                    class="navbar-logo" />Universitas Bali Dwipa
+                </a>
+                <p>Jalan Pulau Flores No.5<br>Denpasar, Bali 80114</p>
+                <p>Email : info@balidwipa.ac.id</p>
+                <p>Whatsapp : 085792463944</p>
+                <p>Phone : 081339827770</p>
+                <p><small>Icons and Pictures by: <a href="https://www.flaticon.com">www.flaticon.com</a> and <a href="https://www.freepik.com"> www.freepik.com</a></small></p>
+                <p><small>@ 2020 Universitas Bali Dwipa </small></p>
+            </div>
+            </div>
+
+            <div class="row mt-4">
+            <div class="col text-center">
+                <p class="lead">
+                <a href="https://www.facebook.com/ubadbali/" class="mx-2"><i class="fab fa-facebook" aria-hidden="true"></i></a>
+                <a href="https://www.instagram.com/universitasbalidwipa/" class="mx-2"><i class="fab fa-instagram" aria-hidden="true"></i></a>
+                <a href="#" class="mx-2"><i class="fab fa-google" aria-hidden="true"></i></a>
+                </p>
+            </div>
+            </div>
+        </div>
+        </footer>
+   
+
+    <!-- Optional JavaScript -->
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js" defer></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js" defer></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    
+    <script>
+        AOS.init();
+    </script>
+
+<!-- Getbutton.io widget -->
+<script type="text/javascript">
+    (function () {
+        var options = {
+            whatsapp: "+6285792463944", // WhatsApp number
+            call_to_action: "Message us", // Call to action
+            position: "right", // Position may be 'right' or 'left'
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
+<!-- /Getbutton.io widget -->
+
+<!--end datatable-->
+</body>
+
+</html>
