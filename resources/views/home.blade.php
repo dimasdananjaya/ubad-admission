5c896d2183917f75d71c1693bf5b458af53542d7
dimasdananjaya <gededimas@gmail.com>
2020-12-04 16:10:58 +0800

    designing Gelombang Pendaftaran

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,23 +2,63 @@
 
 @section('content')
 <section id="mahasiswa-baru">
-    <div data-aos="fade-left" class="col-lg-12 maba-header">
-        <h1 data-aos="fade-left" class="text-center"><b>Pendaftaran Mahasiswa Baru Dibuka !</b></h1>
-        <h2 data-aos="fade-left" class="text-center"><b>GELOMBANG EARLY BIRD 2020/2021</b></h2>
-        <hr>
-        <p style="text-align:justify" class="text-center card-text" data-aos="fade-left" class="why-dosen lead">
-            Daftarkan dirimu sekarang untuk bergabung dengan Universitas Bali Dwipa,
-            10 Program Studi pilihan tersedia untukmu. <br>Ikuti tata cara pendaftaran dibawah untuk melakukan pendaftaran
-        <p>
-
-        <img class="mx-auto d-block hero-image" src="/resources/logo/mahasiswa-baru.svg">
+    <div data-aos="fade-left">
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+              <h1 class="text-center"><b>PENDAFTARAN MAHASISWA BARU</b></h1>
+              <h3 class="text-center"><b>TAHUN AKADEMIK 2021/2022</b></h3>
+            </div>
+         </div>
     </div>
+
+    <div id="gelombang-pendaftaran">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2 class="text-center"><b>GELOMBANG EARLY BIRD</b></h2>
+                    <p class="text-center"><b>Daftar Sekarang Kuliah Nanti, Dengan Keuntungan Berikut :</b></p>
+                </div><!--col-lg-12-->
+                <div class="col-lg-4">
+                    <div class="card">
+                        <img class="card-img-top" src="/resources/logo/potongan.svg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title"><b>Potongan Biaya Kuliah</b></h5>
+                            <p class="card-text">Isi formulir online dengan menekan tombol dibawah, pastikan data yang anda isi sesuai, Setelah mengisi formulir secara online admin kami akan memproses formulir dan akan menghubungi via Whatsapp
+                                dalam waktu 24 jam</p>
+                        </div><!--card body-->
+                    </div><!--card-->     
+                </div><!--col-lg-4-->
+
+                <div class="col-lg-4">
+                    <div class="card">
+                        <img class="card-img-top" src="/resources/logo/beasiswa.svg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title"><b>Beasiswa Rp. 2jt</b></h5>
+                            <p class="card-text">Isi formulir online dengan menekan tombol dibawah, pastikan data yang anda isi sesuai, Setelah mengisi formulir secara online admin kami akan memproses formulir dan akan menghubungi via Whatsapp
+                                dalam waktu 24 jam</p>
+                        </div><!--card body-->
+                    </div><!--card-->     
+                </div><!--col-lg-4-->
+
+                <div class="col-lg-4">
+                    <div class="card">
+                        <img class="card-img-top" src="/resources/logo/gratis-pendaftaran.svg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title"><b>Gratis Pendaftaran</b></h5>
+                            <p class="card-text">Isi formulir online dengan menekan tombol dibawah, pastikan data yang anda isi sesuai, Setelah mengisi formulir secara online admin kami akan memproses formulir dan akan menghubungi via Whatsapp
+                                dalam waktu 24 jam</p>
+                        </div><!--card body-->
+                    </div><!--card-->     
+                </div><!--col-lg-4-->
+            </div><!--row-->
+        </div><!--container-->
+    </div><!--gelombang pendaftaran-->
 
     <div id="merdeka-belajar">
         <div class="container">
             <h3 class="text-center mb-3 mt-5"><b>Kampus Merdeka, Merdeka Belajar !</b></h3>
             <p class="text-center mb-3">Dalam mendukung program Merdeka Belajar oleh Kementerian Pendidikan dan Kebudayaan Republik Indonesia dan mensupport 
-                calon mahasiswa baru <br> dalam masa Pandemi COVID-19 ini Universitas Bali Dwipa
+                calon mahasiswa baru dalam masa Pandemi COVID-19 ini Universitas Bali Dwipa
                 memberikan <b>5 Kebebasan</b> bagi Calon Mahasiswa Baru diantaranya : </p>
             <div class="row">
                 <div class="col-lg-4 mt-5">
@@ -89,101 +129,102 @@
         </div><!--container-->
     </div><!--merdeka belajar-->
 
-    <div class="container">
-        <!-- Full Page Image Header with Vertically Centered Content -->
-        <div class="row">
-            <div class="col-lg-12">
-                <hr>
-                <h3 class="text-center mt-4"><b>Alur Pendaftaran</b></h3>
-                <img class="img-responsive" src="/resources/logo/alur-pendaftaran.svg" alt="Card image cap">
-            </div><!--col 12-->
-
-            <div class="col-lg-6">
-                <div class="card">
-                    <img class="card-img-top" src="/resources/logo/isi-formulir.svg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title"><b>1. Isi Formulir Online</b></h5>
-                        <p class="card-text">Isi formulir online dengan menekan tombol dibawah, pastikan data yang anda isi sesuai, Setelah mengisi formulir secara online admin kami akan memproses formulir dan akan menghubungi via Whatsapp
-                            dalam waktu 24 jam</p>
-                        <a href="/form-pendaftaran" class="btn btn-primary btn-md text-center">Isi Formulir Pendaftaran Disini</a>
-                    </div><!--card body-->
-                </div><!--card-->
-            </div><!--col 6-->
-
-            <div class="col-lg-6">
-                <div class="card">
-                    <img class="card-img-top" src="/resources/logo/admin.svg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title"><b>*Admin Akan Segera Menghubungi Anda*</b></h5>
-                        <p class="card-text">
-                            Setelah mengisi formulir Online maka salah satu dari admin kami akan menghubungi anda, admin kami nantinya
-                            akan membantu anda hingga proses akhir pendaftaran
-                        </p>
-                    </div><!--card body-->
-                </div><!--card-->
-            </div><!--col 6-->
-
-            <div class="col-lg-6">
-                <div class="card">
-                    <img class="card-img-top" src="/resources/logo/diproses.svg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title"><b>2. Penyerahan Persyaratan</b></h5>
-                        <p class="card-text">
-                            Calon manahasiswa baru menyerahkan kelengkapan berkas persyaratan pendaftaran berupa : <br>
-                            1. Ijazah (Legalisir)<br>
-                            2. Rapor (Legalisir)<br>
-                            3. KTP
-                            <br>
-                            <small><b>*seluruh persyaratan dikirim ke email pmb@balidwipa.ac.id </b></small>
-                        </p>
-                        <a href="/status-pendaftaran" class="btn btn-primary btn-md text-center">Cek Status Pendaftaran</a>
-                    </div><!--card body-->
-                </div><!--card-->
-            </div><!--col 6-->
-
-            <div class="col-lg-6">
-                <div class="card">
-                    <img class="card-img-top" src="/resources/logo/pembayaran.svg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title"><b>3. Pembayaran Pendaftaran dan Initial Payment</b></h5>
-                        <p class="card-text">Setelah data divalidasi selanjutnya calon mahasiswa wajib membayar <b>Biaya Pendaftaran dan Initial Payment</b>
-                        jumlah biaya bergantung pada Program Studi yang dipilih, hubungi admin kami dengan menekan tombol Whatsapp di pojok kanan bawah
-                        untuk mengetahui biaya yang harus dibayar. Bukti pembayaran dapat dibawa langsung ke Universitas Bali Dwipa atau dikirimkan online melalui admin kami
-                        yang telah menghubungi anda.
-                        <br><small><b>*tata cara pembayaran akan diberikan setelah mengumpulkan berkas pendaftaran kepada admin</b></small>
-                        </p>
-                    </div><!--card body-->
-                </div><!--card-->
-            </div><!--col 6-->
-
-            <div class="col-lg-6">
-                <div class="card">
-                    <img class="card-img-top" src="/resources/logo/registrasi-ulang.svg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title"><b>4. Registrasi Ulang</b></h5>
-                        <p class="card-text">Setelah melakukan <b>Pembayaran Pendaftaran dan Initial Payment (3)</b> selanjutnya calon mahasiswa wajib melakukan <b>Registrasi Ulang</b>
-                        Registrasi Ulang adalah proses administrasi yang <b>Wajib</b> dilakukan oleh seluruh calon mahasiswa, proses ini mencakup melengkapi administrasi, pengumpulan
-                        dokumen, dan pelunasan DP3 dan Semester. 
-                        </p>
-                    </div><!--card body-->
-                </div><!--card-->
-            </div><!--col 6-->
-
-            <div class="col-lg-6 d-flex mx-auto">
-                <div class="card">
-                    <img class="card-img-top" src="/resources/logo/selesai.svg" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title" style="color : #4e89ae;"><b>5. SELESAI</b></h5>
-                        <p class="card-text">Setelah calon Mahasiswa Baru menyelesaikan seluruh proses 1-4, maka <b>SELAMAT !</b> Anda telah terdaftar resmi sebagai Calon Mahasiswa Universitas Bali Dwipa. 
-                            Calon mahasiswa menunggu untuk pengumuman Masa Orientasi</p>
-                        <a href="/status-pendaftaran" class="btn btn-primary btn-md text-center">Cek Status Pendaftaran</a>
-                    </div><!--card body-->
-                </div><!--card-->
-            </div><!--col 6-->
-
-            
-        </div><!--row-->
-    </div><!--container-->
+    <div id="alur-pendaftaran">
+        <div class="container">
+            <!-- Full Page Image Header with Vertically Centered Content -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <hr>
+                    <h3 class="text-center mt-4"><b>Alur Pendaftaran</b></h3>
+                    <img class="img-responsive" src="/resources/logo/alur-pendaftaran.svg" alt="Card image cap">
+                </div><!--col 12-->
+    
+                <div class="col-lg-6">
+                    <div class="card">
+                        <img class="card-img-top" src="/resources/logo/isi-formulir.svg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title"><b>1. Isi Formulir Online</b></h5>
+                            <p class="card-text">Isi formulir online dengan menekan tombol dibawah, pastikan data yang anda isi sesuai, Setelah mengisi formulir secara online admin kami akan memproses formulir dan akan menghubungi via Whatsapp
+                                dalam waktu 24 jam</p>
+                            <a href="/form-pendaftaran" class="btn btn-primary btn-md text-center">Isi Formulir Pendaftaran Disini</a>
+                        </div><!--card body-->
+                    </div><!--card-->
+                </div><!--col 6-->
+    
+                <div class="col-lg-6">
+                    <div class="card">
+                        <img class="card-img-top" src="/resources/logo/admin.svg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title"><b>*Admin Akan Segera Menghubungi Anda*</b></h5>
+                            <p class="card-text">
+                                Setelah mengisi formulir Online maka salah satu dari admin kami akan menghubungi anda, admin kami nantinya
+                                akan membantu anda hingga proses akhir pendaftaran
+                            </p>
+                        </div><!--card body-->
+                    </div><!--card-->
+                </div><!--col 6-->
+    
+                <div class="col-lg-6">
+                    <div class="card">
+                        <img class="card-img-top" src="/resources/logo/diproses.svg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title"><b>2. Penyerahan Persyaratan</b></h5>
+                            <p class="card-text">
+                                Calon manahasiswa baru menyerahkan kelengkapan berkas persyaratan pendaftaran berupa : <br>
+                                1. Ijazah (Legalisir)<br>
+                                2. Rapor (Legalisir)<br>
+                                3. KTP
+                                <br>
+                                <small><b>*seluruh persyaratan dikirim ke email pmb@balidwipa.ac.id </b></small>
+                            </p>
+                            <a href="/status-pendaftaran" class="btn btn-primary btn-md text-center">Cek Status Pendaftaran</a>
+                        </div><!--card body-->
+                    </div><!--card-->
+                </div><!--col 6-->
+    
+                <div class="col-lg-6">
+                    <div class="card">
+                        <img class="card-img-top" src="/resources/logo/pembayaran.svg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title"><b>3. Pembayaran Pendaftaran dan Initial Payment</b></h5>
+                            <p class="card-text">Setelah data divalidasi selanjutnya calon mahasiswa wajib membayar <b>Biaya Pendaftaran dan Initial Payment</b>
+                            jumlah biaya bergantung pada Program Studi yang dipilih, hubungi admin kami dengan menekan tombol Whatsapp di pojok kanan bawah
+                            untuk mengetahui biaya yang harus dibayar. Bukti pembayaran dapat dibawa langsung ke Universitas Bali Dwipa atau dikirimkan online melalui admin kami
+                            yang telah menghubungi anda.
+                            <br><small><b>*tata cara pembayaran akan diberikan setelah mengumpulkan berkas pendaftaran kepada admin</b></small>
+                            </p>
+                        </div><!--card body-->
+                    </div><!--card-->
+                </div><!--col 6-->
+    
+                <div class="col-lg-6">
+                    <div class="card">
+                        <img class="card-img-top" src="/resources/logo/registrasi-ulang.svg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title"><b>4. Registrasi Ulang</b></h5>
+                            <p class="card-text">Setelah melakukan <b>Pembayaran Pendaftaran dan Initial Payment (3)</b> selanjutnya calon mahasiswa wajib melakukan <b>Registrasi Ulang</b>
+                            Registrasi Ulang adalah proses administrasi yang <b>Wajib</b> dilakukan oleh seluruh calon mahasiswa, proses ini mencakup melengkapi administrasi, pengumpulan
+                            dokumen, dan pelunasan DP3 dan Semester. 
+                            </p>
+                        </div><!--card body-->
+                    </div><!--card-->
+                </div><!--col 6-->
+    
+                <div class="col-lg-6 d-flex mx-auto">
+                    <div class="card">
+                        <img class="card-img-top" src="/resources/logo/selesai.svg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title" style="color : #4e89ae;"><b>5. SELESAI</b></h5>
+                            <p class="card-text">Setelah calon Mahasiswa Baru menyelesaikan seluruh proses 1-4, maka <b>SELAMAT !</b> Anda telah terdaftar resmi sebagai Calon Mahasiswa Universitas Bali Dwipa. 
+                                Calon mahasiswa menunggu untuk pengumuman Masa Orientasi</p>
+                            <a href="/status-pendaftaran" class="btn btn-primary btn-md text-center">Cek Status Pendaftaran</a>
+                        </div><!--card body-->
+                    </div><!--card-->
+                </div><!--col 6-->
+    
+            </div><!--row-->
+        </div><!--container-->
+    </div><!--alur pendaftaran-->
 
     <section id="form-pendaftaran">
         <div class="container">
